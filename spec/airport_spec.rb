b49dfb35d1dd92a45c4b902d27bce0e0b0dee26f
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-12-12 12:57:06 +0000

    Airport Challenge: Version 2

---
+++ spec/airport_spec.rb
@@ -0,0 +1,21 @@
+require 'airport'
+require 'plane'
+
+# A plane currently in the airport can be requested to take off.
+#
+# No more planes can be added to the airport, if it's full.
+# It is up to you how many planes can land in the airport and how that is impermented.
+#
+# If the airport is full then no planes can land
+
+describe Airport do
+
+  let(:airport) { Airport.new }
+
+  context 'taking off and landing' do
+
+    it 'a plane can land' do
+
+    end
+  end
+end 
